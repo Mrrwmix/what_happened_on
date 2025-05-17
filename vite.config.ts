409c38e6747f67a690de37044ecd402d81247166
Mrrwmix <32693786+Mrrwmix@users.noreply.github.com>
2025-05-17 10:07:59 -0500

    deploy updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/what_happened_on/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
   test: {
     globals: true,
     environment: "jsdom",
